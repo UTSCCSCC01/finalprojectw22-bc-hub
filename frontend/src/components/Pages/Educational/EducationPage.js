47e444399b6d6cf979c5d0c6402e43044e3f4367
Aiden <edgex1024@gmail.com>
2022-02-07 13:07:43 -0500

    minor gix of styling

--- frontend/src/components/Pages/Educational/EducationPage.js
+++ frontend/src/components/Pages/Educational/EducationPage.js
@@ -19,70 +19,90 @@ function EducationPage() {
       <NavBar/>
       <Container className='ctr'> 
         <Row >
-          <Col>Beginner</Col> 
+          <Col><h1>Beginner</h1></Col> 
 
           <Col className='col'>
               <a href='https://www.youtube.com/watch?v=6Gu2QMTAkEU' target={'blank'}>
               <img src={r1c1} alt='' width="200px" height={"150px"} />   
               </a>
-              <p>  What is Crypto Currency ?</p>
+              <br/>
+              <br/>
+              <p>  What is Crypto Currency</p>
           </Col> 
           
           
           <Col>
-          <a href='/www.google.com'>
+          <a href='https://www.fool.com/the-ascent/cryptocurrency/best-cryptocurrency-apps/' target={'blank'}>
             <img src={r1c2} alt='' width="200px" height={"150px"} />
           </a>
-          <p>  What is Crypto Currency ?</p>
+          <br/>
+          <br/>
+          <p>     Beginner Crypto-App</p>
           </Col> 
           
           <Col>
-          <a href='/www.google.com'>
+          <a href='https://coinmarketcap.com/trending-cryptocurrencies/' target={'blank'}>
             <img src={r1c3} alt='' width="200px" height={"150px"} />
           </a>
-          <p>  What is Crypto Currency ?</p>
+          <br/>
+          <br/>
+          <p>  How to Find Crypto Trend</p>
           </Col>
         </Row>
 
         <Row>
-          <Col>Intermediate</Col> 
+          <Col><h1>Intermediate</h1></Col> 
           
-          <Col><a href='/www.google.com'>
+          <Col><a href='https://www.investopedia.com/tech/most-important-cryptocurrencies-other-than-bitcoin/' target={'blank'}>
             <img src={r2c1} alt='' width="200px" height={"150px"} />
           </a>
-          <p>  What is Crypto Currency ?</p>
+          <br/>
+          <br/>
+          <p> Investment Choice</p>
           </Col> 
           
-          <Col><a href='/www.google.com'>
+          <Col><a href='https://thedailyguardian.com/impact-of-cryptocurrencies-on-the-economy/' target={'blank'}>
             <img src={r2c2} alt='' width="200px" height={"150px"} />
           </a>
-          <p>  What is Crypto Currency ?</p>
+          <br/>
+          <br/>
+          <p>  Global Economical Effect</p>
           </Col> 
           
-          <Col><a href='/www.google.com'>
+          <Col><a href='https://www.sofi.com/learn/content/understanding-the-different-types-of-cryptocurrency/' target={'blank'}>
             <img src={r2c3} alt='' width="200px" height={"150px"} />
           </a>
-          <p>  What is Crypto Currency ?</p>
+          <br/>
+          <br/>
+          <p> Types of Digital Currencies</p>
           </Col>
         </Row>
 
         <Row>
-          <Col>Advanced</Col> <Col><a href='/www.google.com'>
+          <Col><h1>Advanced</h1></Col> 
+          
+          <Col><a href='https://www.youtube.com/watch?v=422HORNUfkU' target={'blank'}>
             <img src={r3c1} alt='' width="200px" height={"150px"} />
           </a>
-          <p>  What is Crypto Currency ?</p>
+          <br/>
+          <br/>
+          <p>         Crypto v. Token</p>
           </Col> 
           
-          <Col><a href='/www.google.com'>
+          <Col><a href='https://www.cmcmarkets.com/en/learn-cryptocurrencies/what-are-the-risks' target={'blank'}>
             <img src={r3c2} alt='' width="200px" height={"150px"} />
           </a>
-          <p>  What is Crypto Currency ?</p>
+          <br/>
+          <br/>
+          <p>      Risk of Crypto Trade</p>
           </Col> 
           
-          <Col><a href='/www.google.com'>
+          <Col><a href='https://www.youtube.com/watch?v=SSo_EIwHSd4' target={'blank'}>
             <img src={r3c3} alt='' width="200px" height={"150px"} />
           </a>
-          <p>  What is Crypto Currency ?</p>
+          <br/>
+          <br/>
+          <p>              Blockchain</p>
           </Col>
         </Row>
 
