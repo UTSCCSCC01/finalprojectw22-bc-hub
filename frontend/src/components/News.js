dcdb24f03490a3f9ca6fb3b3e06a71b48502bd06
Faraz <123faraz123@gmail.com>
2022-02-07 19:03:23 -0500

    [BH-27] Update article component to use real data

--- frontend/src/components/News.js
+++ frontend/src/components/News.js
@@ -4,21 +4,25 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image'
 
-function News({title, preview, publisher, date, img}) {
+function News({title, preview, publisher, date, img, link}) {
   return (
     <Container>
       <Row className='justify-content-center mb-2'>
           <Col className='border-bottom' xs={6}>
+            <a target="_blank" rel="noopener noreferrer" className="text-decoration-none text-black" href={link}>
               <h4 className=''>{title}</h4>
               <p className='mb-1'>{preview}</p>
-              <p className='fw-bold'>{publisher}&nbsp;&nbsp;&nbsp;&nbsp;{date}</p>     
+              <p className='fw-bold'>{publisher}&nbsp;&nbsp;&nbsp;&nbsp;{date}</p>    
+            </a> 
           </Col>
           <Col xs={2} className='border-bottom d-flex align-items-center justify-content-center p-2'>
-            <Image className="h-100" rounded='true' src={img}>
-            </Image>
+            <a target="_blank" rel="noopener noreferrer" href={link}>
+              <Image className="h-100 w-100" rounded='true' src={img}></Image>
+            </a>
           </Col>
       </Row>
     </Container>
+    
   );
 }
 
