760d742720f8c716eb95b8d198b7dc68e0ecf1af
Faraz <123faraz123@gmail.com>
2022-02-09 02:52:47 -0500

    [BH-27] Add browser router and route for newsfeed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import GrabNews from './components/GrabNews';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const rootElement = document.getElementById('root');
+render(
+  <BrowserRouter>
+    <Routes>
+    <Route path="/" element={<App />} />
+      <Route path="/newsfeed" element={<GrabNews/>} />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
