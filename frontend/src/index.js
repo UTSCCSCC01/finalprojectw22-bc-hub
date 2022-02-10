cf013ef0fbea454561f41cb78351ee92829dda8d
Faraz <123faraz123@gmail.com>
2022-02-09 20:48:09 -0500

    Undo merge of feature/BH-7

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { render } from "react-dom";
 import App from "./App";
 import './index.css';
+import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
 // import files
 
 import EducationPage from './components/Pages/Educational/EducationPage';
-import GrabNews from './components/GrabNews';
+import News from './components/Pages/News/NewsPage';
 import Market from './components/Pages/Market/MarketPage';
 import Community from './components/Pages/Community/CommunityPage';
 
@@ -21,7 +21,7 @@ render(
   <Routes>
     <Route path="/" element={<App />} />
     <Route path="education" element={<EducationPage />} />
-    <Route path="news" element={<GrabNews />} />
+    <Route path="news" element={<News />} />
     <Route path="market" element={<Market />} />
     <Route path="community" element={<Community />} />
   </Routes>
@@ -29,3 +29,7 @@ render(
   rootElement
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
