7c073833001fe0954f87a4afda71ed7ead22f568
Faraz <123faraz123@gmail.com>
2022-02-09 20:37:48 -0500

    Reverting to commit before merge

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import { render } from "react-dom";
-import App from "./App";
+import {render} from 'react-dom';
 import './index.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-// import files
-
-import EducationPage from './components/Pages/Educational/EducationPage';
+import App from './App';
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 import GrabNews from './components/GrabNews';
-import Market from './components/Pages/Market/MarketPage';
-import Community from './components/Pages/Community/CommunityPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 render(
   <BrowserRouter>
-  <Routes>
+    <Routes>
     <Route path="/" element={<App />} />
-    <Route path="education" element={<EducationPage />} />
-    <Route path="news" element={<GrabNews />} />
-    <Route path="market" element={<Market />} />
-    <Route path="community" element={<Community />} />
-  </Routes>
-</BrowserRouter>,
+      <Route path="/newsfeed" element={<GrabNews/>} />
+    </Routes>
+  </BrowserRouter>,
   rootElement
 );
 
