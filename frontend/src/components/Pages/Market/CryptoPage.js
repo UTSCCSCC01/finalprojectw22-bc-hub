459fb8eccfa3e01a52b0e2b8ce67a09d9254e71a
natha <nathan.ralph122@gmail.com>
2022-02-10 02:13:43 -0500

    [BH-30] Display data on frontend

---
+++ frontend/src/components/Pages/Market/CryptoPage.js
@@ -0,0 +1,30 @@
+import React, { Component, useEffect, useState } from 'react';
+import NavBar from '../../NavBar/NavBar';
+import useFetch  from '../../../hooks/useFetch';
+
+
+function CryptoCoin() {
+
+  const url = window.location.href;
+
+  const {data: marketData, isLoading, error} = useFetch("http://localhost:5000/market/main" + url.substring(url.lastIndexOf('/')));
+
+  if (isLoading) {
+    return(<div>loading</div>)
+  }
+
+  return <div id="crypto_page">
+      <NavBar/>
+      <h1>Cryptocurrency Data</h1>
+      <p> Name: {marketData.name}</p>
+      <p> Symbol: {marketData.symbol}</p>
+      <p> Price: ${marketData.quote.USD.price}</p>
+      <p> 24h Change: {marketData.quote.USD.percent_change_24h}%</p>
+      <p> 7d Change: {marketData.quote.USD.percent_change_7d}%</p>
+      <p> 30d Change: {marketData.quote.USD.percent_change_30d}%</p>
+      <p> Market Cap: ${marketData.quote.USD.market_cap}</p>
+
+  </div>;
+}
+
+export default CryptoCoin;
--- frontend/src/components/Pages/Market/MarketPage.js
+++ frontend/src/components/Pages/Market/MarketPage.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import NavBar from '../../NavBar/NavBar';
+import useFetch  from '../../../hooks/useFetch';
+import Table from 'react-bootstrap/Table'
+import './MarketPage.css'
 
 function Market() {
-  return <div>
+
+
+  const {data: marketData, isLoading, error} = useFetch("http://localhost:5000/market/main");
+
+
+  if (isLoading) {
+    return(<div>loading</div>)
+  }
+  
+  return <div id="market_page">
       <NavBar/>
       <h1>Market Section</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Coin</th>
+            <th>Price (USD)</th>
+            <th>24h</th>
+            <th>7d</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from({length: marketData.data.length}).map((_, index1) => (
+            <tr>
+              <td><a href={"cryptocoin/" + marketData.data[index1].symbol}>{marketData.data[index1].symbol}</a></td>
+                <td>{marketData.data[index1].quote.USD.price}</td>
+                <td>{marketData.data[index1].quote.USD.percent_change_24h}</td>
+                <td>{marketData.data[index1].quote.USD.percent_change_7d}</td>
+            </tr>
+          ))}
+        </tbody>
+
+      </Table>
+      
+
   </div>;
 }
 
