a41ed5149e88a42b45aee0bb95f4edd6b01bf1f5
natha <nathan.ralph122@gmail.com>
2022-02-28 20:38:10 -0500

    Implemented Searchbar with button funciton.

--- frontend/src/components/Pages/Market/MarketPage.js
+++ frontend/src/components/Pages/Market/MarketPage.js
@@ -1,44 +1,57 @@
 import NavBar from '../../NavBar/NavBar';
+import SearchBar from '../../SearchBar/SearchBar';
 import useFetch  from '../../../hooks/useFetch';
 import Table from 'react-bootstrap/Table'
 import './MarketPage.css'
 
+function test(param) {
+  const inVal = document.getElementById('my-input').value;
+  for (var i = 0; i < param.length; i++){
+    if (param[i].symbol == inVal){
+      window.location = "/" + inVal;
+    }
+  }
+
+
+}
+
 function Market() {
  
-
   const {data: marketData, isLoading, error} = useFetch("http://localhost:5000/market/main");
 
 
   if (isLoading) {
     return(<div></div>)
   }
-  
+
   return <div id="market_page">
       <NavBar/>
       <h1>Market Section</h1>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>Coin</th>
-            <th>Price (USD)</th>
-            <th>24h</th>
-            <th>7d</th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.from({length: marketData.data.length}).map((_, index1) => (
+      <div id="datatable">
+      <SearchBar fun={test} param={marketData.data} inVal={document.getElementById("my-input")}/>
+
+        <Table striped bordered hover id="market-table">
+          <thead>
             <tr>
-              <td><a href={"/" + marketData.data[index1].symbol}>{marketData.data[index1].symbol}</a></td>
-                <td>{marketData.data[index1].quote.USD.price}</td>
-                <td>{marketData.data[index1].quote.USD.percent_change_24h}</td>
-                <td>{marketData.data[index1].quote.USD.percent_change_7d}</td>
+              <th>Coin</th>
+              <th>Price (USD)</th>
+              <th>24h</th>
+              <th>7d</th>
             </tr>
-          ))}
-        </tbody>
-
-      </Table>
+          </thead>
+          <tbody>
+            {Array.from({length: marketData.data.length}).map((_, index1) => (
+              <tr>
+                <td><a href={"/" + marketData.data[index1].symbol}>{marketData.data[index1].symbol}</a></td>
+                  <td>{marketData.data[index1].quote.USD.price}</td>
+                  <td>{marketData.data[index1].quote.USD.percent_change_24h}</td>
+                  <td>{marketData.data[index1].quote.USD.percent_change_7d}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
       
-
   </div>;
 }
 
---
+++ frontend/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import './SearchBar.css'
+
+function SearchBar(props){
+    return (
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Enter symbol" aria-label="Search" aria-describedby="basic-addon2" id="my-input"/>
+            <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="button" onClick={()=>props.fun(props.param)}>Search</button>
+            </div>
+        </div>
+    );
+    
+}
+export default SearchBar;
