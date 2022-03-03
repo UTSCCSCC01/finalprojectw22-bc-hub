b53e5c7b68756f8771228c0c054ffb685f711545
natha <nathan.ralph122@gmail.com>
2022-03-02 19:52:54 -0500

    Add followed crypto to follow table

--- frontend/src/components/Pages/Market/MarketPage.js
+++ frontend/src/components/Pages/Market/MarketPage.js
@@ -4,6 +4,8 @@ import useFetch  from '../../../hooks/useFetch';
 import Table from 'react-bootstrap/Table'
 import './MarketPage.css'
 
+const followArr = [];
+
 function searchFun(param) {
   const inVal = document.getElementById('my-input').value;
   const txtVal = inVal.toUpperCase();
@@ -34,16 +36,26 @@ function filterFun(param){
   }
 }
 
-function butFun(bText){
-  const but = document.getElementById('table-but' + bText)
+function butFun(rowNum, marketData){
+  const but = document.getElementById('table-but' + rowNum);
+  const followTab = document.getElementById('follow-table');
+  
   if (but.value == 'follow'){
     but.value = 'unfollow';
     but.innerText = 'unfollow';
+    followArr.push(marketData[rowNum]);
+
+    const row = followTab.insertRow(0);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+
+    cell1.innerHTML = marketData[rowNum].symbol;
+    cell2.innerHTML = marketData[rowNum].quote.USD.price;
+    
   } else {
     but.value ='follow';
     but.innerText = 'follow';
   }
-  
 }
 
 function Market() {
@@ -57,6 +69,15 @@ function Market() {
   return <div id="market_page">
       <NavBar/>
       <h1>Market Section</h1>
+      <div id='follow-section'>
+        <h3>Followed Currencies</h3>
+        <Table striped bordered hover id='follow-table'>
+          <thead>
+            <th>Coin</th>
+            <th>Price</th>
+          </thead>
+        </Table>
+      </div>
       <div id="datatable">
       <SearchBar butFun={searchFun} param={marketData.data} inVal={document.getElementById("my-input")} inFun={filterFun}
       text={"Enter Symbol"}/>
@@ -78,7 +99,7 @@ function Market() {
                   <td>{marketData.data[index1].quote.USD.percent_change_24h}</td>
                   <td>{marketData.data[index1].quote.USD.percent_change_7d}</td>
                   <td id="but-col"><button id={"table-but" + index1} class="btn btn-outline-secondary" 
-                  type="buton" onClick={()=>butFun(index1)} value='follow' innerText='follow'>follow</button></td>
+                  type="buton" onClick={()=>butFun(index1, marketData.data)} value='follow'>follow</button></td>
               </tr>
             ))}
           </tbody>
