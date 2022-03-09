d4a500b7b665cf2c4fd58be04e69b6fa8003b396
Kesavar-Kabilar <kesavarkabilar@gmail.com>
2022-03-09 10:27:12 -0500

    [BH-4] Finished Basic Log In and Register Page

--- frontend/src/components/Pages/LogIn/LogIn.js
+++ frontend/src/components/Pages/LogIn/LogIn.js
@@ -1,5 +1,16 @@
+import NavBar from '../../NavBar/NavBar';
+
 function LogIn() {
-    return(<div>Log In Page</div>)
+    return <div id='login_page'>
+                <NavBar/>
+                <form>
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username"></input><br></br>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password"></input><br></br>
+                    <input type="submit" value="Log In"></input>
+                </form>
+            </div>
 }
 
 export default LogIn;
--- frontend/src/components/Pages/Register/Register.js
+++ frontend/src/components/Pages/Register/Register.js
@@ -1,5 +1,20 @@
+import NavBar from '../../NavBar/NavBar';
+
 function Register() {
-    return(<div>Register Page</div>)
+    return <div id='register_page'>
+                <NavBar/>
+                <form>
+                    <label for="firstname">First Name:</label>
+                    <input type="text" id="firstname" name="firstname"></input><br></br>
+                    <label for="lastname">Last Name:</label>
+                    <input type="text" id="lastname" name="lastname"></input><br></br>
+                    <label for="username">Username:</label>
+                    <input type="text" id="username" name="username"></input><br></br>
+                    <label for="password">Password:</label>
+                    <input type="password" id="password" name="password"></input><br></br>
+                    <input type="submit" value="Sign Up"></input>
+                </form>
+            </div>
 }
 
 export default Register;
