0c245de1f142b28b206716939a941df442618e80
Kesavar-Kabilar <kesavarkabilar@gmail.com>
2022-03-14 12:17:05 -0400

    [BH-39] Finished Styling for Log In Page

--- frontend/src/components/Pages/LogIn/LogIn.js
+++ frontend/src/components/Pages/LogIn/LogIn.js
@@ -1,14 +1,18 @@
 import NavBar from '../../NavBar/NavBar';
+import './LogIn.css'
 
 function LogIn() {
     return <div id='login_page'>
                 <NavBar/>
-                <form>
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="username"></input><br></br>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password"></input><br></br>
-                    <input type="submit" value="Log In"></input>
+                <form class="Main">
+                    <h1 class="text1">BC HUB</h1>
+                    <h2 class="text2">"Bitcoin is a technological tour de force" -- Bill Gates</h2>
+                    <h4 class="border"></h4>
+                    <h3 class="text3">Log In</h3>
+                    <input type="text" id="username" name="username" class="textbox1" placeholder='User Name'></input><br></br>
+                    <input type="password" id="password" name="password" class="textbox2" placeholder='Password'></input><br></br>
+                    <button type="submit" value="Log In" class="button1">Log In</button>
+                    
                 </form>
             </div>
 }
