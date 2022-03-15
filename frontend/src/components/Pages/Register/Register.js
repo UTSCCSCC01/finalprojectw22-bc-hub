c89c4b1de3a440c7fa6a53a66cbd3de8115bffdc
Kesavar-Kabilar <kesavarkabilar@gmail.com>
2022-03-15 09:51:57 -0400

    [BH-84] Finished stylings of register page

--- frontend/src/components/Pages/Register/Register.js
+++ frontend/src/components/Pages/Register/Register.js
@@ -1,18 +1,19 @@
 import NavBar from '../../NavBar/NavBar';
+import './Register.css'
 
 function Register() {
     return <div id='register_page'>
                 <NavBar/>
-                <form>
-                    <label for="firstname">First Name:</label>
-                    <input type="text" id="firstname" name="firstname"></input><br></br>
-                    <label for="lastname">Last Name:</label>
-                    <input type="text" id="lastname" name="lastname"></input><br></br>
-                    <label for="username">Username:</label>
-                    <input type="text" id="username" name="username"></input><br></br>
-                    <label for="password">Password:</label>
-                    <input type="password" id="password" name="password"></input><br></br>
-                    <input type="submit" value="Sign Up"></input>
+                <form class="registermain">
+                    <h2 class="registerborder"></h2>
+                    <h1 class="registertext1">Register</h1>
+                    <input class="registertextbox1" type="text" id="firstname" name="firstname" placeholder='First Name'></input>
+                    <input class="registertextbox2" type="text" id="lastname" name="lastname" placeholder='Last Name'></input>
+                    <input class="registertextbox3" type="text" id="username" name="username" placeholder='User Name'></input>
+                    <input class="registertextbox6" type="text" id="email" name="email" placeholder='Email'></input>
+                    <input class="registertextbox4" type="password" id="password" name="password" placeholder='Password'></input>
+                    <input class="registertextbox5" type="password" id="password" name="password" placeholder='Re-enter Password'></input>
+                    <input class="registerbutton" type="submit" value="Sign Up"></input>
                 </form>
             </div>
 }
