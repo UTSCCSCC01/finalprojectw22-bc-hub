c3af08709f8739c85f4532b5e78340113accf164
natha <nathan.ralph122@gmail.com>
2022-03-16 22:35:59 -0400

    [BH-86] Cleaned up code.

--- frontend/src/components/Pages/Community/Community.js
+++ frontend/src/components/Pages/Community/Community.js
@@ -6,9 +6,6 @@ import NavBar from '../../NavBar/NavBar';
 import Feed from './Feed';
 import CommunitySideBar from './CommunitySidebar';
 import MakePost from './MakePost';
-import SearchBar from '../../SearchBar/SearchBar';
-import filterFunction from './CommunitySearch';
-
 
 const Community = (props) => {
     const {data: communityPosts, isLoading, error}  = useFetch('http://localhost:5000/community/' + props.feed);
