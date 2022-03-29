19c9c33f0e130c7f4ec721c19778c280a2e538f7
Faraz <123faraz123@gmail.com>
2022-03-29 00:44:10 -0400

    [BH-4] Redirect users to login page if they access unauthorized page

--- frontend/src/components/Pages/Community/Community.js
+++ frontend/src/components/Pages/Community/Community.js
@@ -6,8 +6,31 @@ import NavBar from '../../NavBar/NavBar';
 import Feed from './Feed';
 import CommunitySideBar from './CommunitySidebar';
 import MakePost from './MakePost';
+import { useNavigate } from 'react-router-dom';
 
 const Community = (props) => {
+    const navigate = useNavigate()
+    if(props.feed === "personal-feed"){
+        const token = localStorage.getItem('token')
+        if(token) {
+            fetch('http://localhost:5000/loggedIn/', {
+                headers: {
+                    'x-access-token': token,
+                }
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(data => {
+                if(data.status !== 200){
+                    navigate('/login')
+                }
+            })
+            .catch(err => console.log(err))
+        } else {
+            navigate('/login')
+        }   
+    }
     const {data: communityPosts, isLoading, error}  = useFetch('http://localhost:5000/community/' + props.feed);
     const [openModal, setOpenModal] = useState(false);
 
