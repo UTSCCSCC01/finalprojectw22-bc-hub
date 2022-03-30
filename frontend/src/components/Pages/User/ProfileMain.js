ac57567aab4cac46083f1c0c3c431a0a7e2609b6
Aiden <edgex1024@gmail.com>
2022-03-30 12:59:07 -0400

    [BH-110] solve merge problem

--- frontend/src/components/Pages/User/ProfileMain.js
+++ frontend/src/components/Pages/User/ProfileMain.js
@@ -19,7 +19,7 @@ const Profilemain = ({userName, userNickName, userId, postNum, followerNum, foll
         {openModal && <ProfileLink closeModal={setOpenModal} />}
             <Container className='d-flex align-items-center justify-content-center pt-5 ' align={"center"}>
                 <a className=''>
-                    <img className='square mx-5 rounded-cricle btn hov' src={profilePic} alt = 'Not Found'  width={200} height={200} onClick={() => setOpenModal(true)} />
+                    
                 </a>
                 
 
