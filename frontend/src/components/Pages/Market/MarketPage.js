0a5066c2db2bc03bd6f497ef50f6dd942f670dcf
Aiden <edgex1024@gmail.com>
2022-03-31 18:12:59 -0400

    [BH-117] finish market page styling

--- frontend/src/components/Pages/Market/MarketPage.js
+++ frontend/src/components/Pages/Market/MarketPage.js
@@ -59,23 +59,24 @@ function butFun(rowNum, marketDataSymbol){
   if (but.value == 'follow'){
     but.value = 'unfollow';
     but.innerText = 'unfollow';
-    tbrow.style.backgroundColor='purple';
+    tbrow.style.backgroundColor='pink';
 
     
 
-    sendCurData(cryInfo);
+  sendCurData(cryInfo);
+
+    
 
     console.log(marketDataSymbol);
   } else {
     but.value ='follow';
     but.innerText = 'follow';
-    tbrow.style.backgroundColor='rgb(41, 0, 0, 0.7)';
-    sendunFolData(cryInfo);
+    tbrow.style.backgroundColor='white';
+
+  sendunFolData(cryInfo);
+
     console.log(marketDataSymbol);
-    // const rowIndex = followArr.length - 1 - followArr.indexOf(marketData[rowNum])
-    // console.log(followArr.indexOf(marketData[rowNum]));
-    // followTab.deleteRow(rowIndex);
-    // followArr.splice(followArr.indexOf(marketData[rowNum]), 1);
+
   }
 }
 
@@ -91,7 +92,7 @@ function allPaint(marketData, coinLst) {
         } else {
           but.value ='unfollow';
           but.innerText = 'unfollow';
-          tbrow.style.backgroundColor='purple';
+          tbrow.style.backgroundColor='pink';
         }
         // console.log("btn is   " + but);
         
@@ -107,7 +108,7 @@ function Market() {
   const params = useParams();
   const [userLoad, setUserLoad] = useState(true);
   const [userStatus, setUserStatus] = useState(false)
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const {data: marketData, isLoading: mkload, error} = useFetch("http://localhost:5000/market/main");
   var urlName = '';
     	useEffect(() => {
@@ -142,6 +143,7 @@ function Market() {
             setIsLoggedIn({loggedIn: false, user: null})
             // navigate('/logIn', { state: "You must be logged in to view other users' profiles!" })
             setUserLoad(false);
+            
         }
         setUserStatus(true)
         
@@ -154,25 +156,26 @@ function Market() {
       return(<div></div>)
     }
 
-  var coinData = []
+  var coinData = [];
+  var User = null;
   if (isLoggedIn.loggedIn) {
-    var User = isLoggedIn.user;
+    User = isLoggedIn.user;
     coinData = User.followingCryptos;
   }
 
 
 
-  return <div className="main-market-page" id="market_page">
+  return <div className="main-market-page" id="market_page" style={{minHeight:1920}}>
       <NavBar/>
-      <Container className='d-flex align-items-center justify-content-center pt-1'>
+      <Container className='d-flex align-items-center justify-content-center pt-1' >
       <SearchBar butFun={searchFun} param={marketData} inVal={document.getElementById("my-input")} inFun={filterFun}
       text={"Enter Symbol"} haveBut={true}/>
       {/* {isLoggedIn.loggedIn && <Currency isOwner={isOwner} userCurrency={User.followingCryptos} style={{width: 300}} />} */}
       </Container>
       <div id="datatable">
       
-      <Container className='d-flex align-items-center justify-content-center pt-1'>
-        <Table striped bordered hover id="market-table" align={'center'} style={{opacity: 1}}>
+      <Container className='d-flex align-items-center justify-content-center pt-1 shadow-lg'>
+        <Table striped bordered hover id="market-table" align={'center'} style={{opacity: 1}} className='table-lg'>
           <thead style={{backgroundColor: 'black'}}>
             <tr>
             <th><h3><center>Coin</center></h3></th>
@@ -184,8 +187,8 @@ function Market() {
           </thead>
           <tbody>
             {Array.from({length: marketData.length}).map((_, index1) => (
-              <tr id={'table_row' + index1} style={{backgroundColor: 'rgb(41, 0, 0, 0.7)'}}>
-                <td><center><a className="link-color" href={"market/" + marketData[index1].symbol}>{marketData[index1].symbol}</a></center></td>
+              <tr id={'table_row' + index1} style={{backgroundColor: 'white'}}>
+                <td ><center><a className="link-color" href={"market/" + marketData[index1].symbol}>{marketData[index1].symbol}</a></center></td>
                   <td><center><a className="table-text">{marketData[index1].quote.USD.price}</a></center></td>
                   <td><center><a className="table-text">{marketData[index1].quote.USD.percent_change_24h}</a></center></td>
                   <td><center><a className="table-text">{marketData[index1].quote.USD.percent_change_7d}</a></center></td>
